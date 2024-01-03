4faf19695c651053f43c1f7985f6d5079e508e42
Duhan Mo <moduhan95@gmail.com>
2024-01-03 12:24:44 +0900

    first-commit

---
+++ src/main/java/org/oop/food/WoowahanOopApplication.java
@@ -0,0 +1,13 @@
+package org.oop.food;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WoowahanOopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WoowahanOopApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/oop/food/WoowahanOopApplicationTests.java
@@ -0,0 +1,13 @@
+package org.oop.food;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WoowahanOopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
